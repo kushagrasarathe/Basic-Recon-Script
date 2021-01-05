3cc8c2dc929fe722b024e92d063a158571fe7ad3
Kushagra Sarathe <76868364+kushagrasarathe@users.noreply.github.com>
2021-01-05 13:00:20 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -10,15 +10,19 @@ bold="\e[1m"
 
 #Starting message
 echo -e "  
-██████╗░░██████╗██╗░░░██╗░█████╗░██╗░░██╗███████╗██████╗░███████╗██╗░░░░░██╗░█████╗░
-██╔══██╗██╔════╝╚██╗░██╔╝██╔══██╗██║░░██║██╔════╝██╔══██╗██╔════╝██║░░░░░██║██╔══██╗
-██████╔╝╚█████╗░░╚████╔╝░██║░░╚═╝███████║█████╗░░██║░░██║█████╗░░██║░░░░░██║██║░░╚═╝
-██╔═══╝░░╚═══██╗░░╚██╔╝░░██║░░██╗██╔══██║██╔══╝░░██║░░██║██╔══╝░░██║░░░░░██║██║░░██╗
-██║░░░░░██████╔╝░░░██║░░░╚█████╔╝██║░░██║███████╗██████╔╝███████╗███████╗██║╚█████╔╝
-╚═╝░░░░░╚═════╝░░░░╚═╝░░░░╚════╝░╚═╝░░╚═╝╚══════╝╚═════╝░╚══════╝╚══════╝╚═╝░╚════╝░
+------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------
+██████╗  ██████╗██╗   ██╗ █████╗ ██╗  ██╗███████╗██████╗ ███████╗██╗	 ██╗ █████╗ 
+██╔══██╗██╔════╝╚██╗ ██╔╝██╔══██╗██║  ██║██╔════╝██╔══██╗██╔════╝██║	 ██║██╔══██╗
+██████╔╝╚█████╗  ╚████╔╝ ██║  ╚═╝███████║█████╗  ██║  ██║█████╗  ██║	 ██║██║  ╚═╝
+██╔═══╝  ╚═══██╗  ╚██╔╝  ██║  ██╗██╔══██║██╔══╝  ██║  ██║██╔══╝  ██║	 ██║██║  ██╗
+██║     ██████╔╝   ██║   ╚█████╔╝██║  ██║███████╗██████╔╝███████╗███████╗██║╚█████╔╝
+╚═╝     ╚═════╝    ╚═╝    ╚════╝ ╚═╝  ╚═╝╚══════╝╚═════╝ ╚══════╝╚══════╝╚═╝ ╚════╝
+------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------
 "
 printf "\nStarting tool...Please WAIT :)" 
-sleep 3  # sleeping for 3 seconds
+sleep 1.5  # sleeping for 1.5 seconds
 
 printf '\n'
 
@@ -26,9 +30,12 @@ printf '\n'
 echo -e "${green}${bold}Enter domain to enumerate subdomains:- ${white}" ;
 read domain ;
 
-#Creating directory
-if [ ! -d "${domain}" ];then
+#Creating directories
+if [ ! -d "$domain" ];then
 	mkdir $domain
+fi
+if [ ! -d "$domain/gf" ];then
+	mkdir $domain/gf
 fi
 
 #using assetfinder
@@ -48,4 +55,22 @@ rm $domain/temp.txt
 echo "[+] Gathering alive domains..."
 cat $domain/final.txt | httprobe > $domain/alive.txt
 
+#running waybackurls and filtering the output
+echo "[+] Collecting endpoints using waybackurls..."
+echo "[+] Filtering the collected endpoints..."
+cat $domain/alive.txt | waybackurls  | grep "=" | egrep -iv ".(jpg|js|jpeg|gif|css|tiff|tif|png|ttf|woff2|woff|ico|pdf|svg|txt)" > $domain/filtered.txt
 
+#gathering gf patterns
+echo "[+] Collecting gf patterns..."
+cat $domain/filtered.txt | gf xss > $domain/gf/xss.txt 
+cat $domain/filtered.txt | gf ssrf > $domain/gf/ssrf.txt 
+cat $domain/filtered.txt | gf ssti > $domain/gf/ssti.txt 
+cat $domain/filtered.txt | gf redirect > $domain/gf/redirect.txt 
+cat $domain/filtered.txt | gf rce > $domain/gf/rce.txt 
+cat $domain/filtered.txt | gf sqli > $domain/gf/sqli.txt 
+cat $domain/filtered.txt | gf lfi > $domain/gf/lfi.txt 
+cat $domain/filtered.txt | gf debug_logic > $domain/gf/debug_logic.txt 
+cat $domain/filtered.txt | gf idor > $domain/gf/idor.txt 
+cat $domain/filtered.txt | gf img-traversal > $domain/gf/img-traversal.txt 
+cat $domain/filtered.txt | gf interestingparams > $domain/gf/interestingparams.txt 
+cat $domain/filtered.txt | gf interestingsubs > $domain/gf/interestingsubs.txt 
